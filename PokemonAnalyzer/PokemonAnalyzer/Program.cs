abaafe84b657405e8c74dee990b1fba78db4e59a
Robert Kasper <98301621+RHKasper@users.noreply.github.com>
2022-05-14 01:19:43 -0400

    Add Stopwatch

--- PokemonAnalyzer/PokemonAnalyzer/Program.cs
+++ PokemonAnalyzer/PokemonAnalyzer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 
@@ -9,11 +10,15 @@ namespace PokemonAnalyzer
 		private const string PokeApi = "https://pokeapi.co/api/v2";
 		static void Main(string[] args)
 		{
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			
 			int limit = Convert.ToInt32(args[0]);
 			int offset = Convert.ToInt32(args[1]);
 			
 			Console.WriteLine("Limit: " + limit + ". Offset: " + offset);
 			RequestPokemonData(limit, offset);
+			
+			Console.WriteLine($"Program took {stopwatch.Elapsed.TotalSeconds} seconds.");
 			Console.WriteLine("Press enter exit");
 			Console.Read();
 		}
@@ -21,12 +26,12 @@ namespace PokemonAnalyzer
 		static void RequestPokemonData(int limit, int offset)
 		{
 			string uri = $"{PokeApi}/pokemon?limit={limit}&offset={offset}";
-			Console.WriteLine(Get(uri));
+			Console.WriteLine(RunWebRequest(uri));
 		}
 		
 		
 		// Todo: find the source of this code. I got it from my BeatSaberUnzipper project, and unfortunately didn't save the link to the source
-		public static string Get(string uri)
+		public static string RunWebRequest(string uri)
 		{
 			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
 			request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
