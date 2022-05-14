f30f5b66a1ef7d5d74adb48ab99bbc3ff6c882e8
Robert Kasper <98301621+RHKasper@users.noreply.github.com>
2022-05-14 01:17:06 -0400

    Access Pokemon API and print all pokemon

--- PokemonAnalyzer/PokemonAnalyzer/Program.cs
+++ PokemonAnalyzer/PokemonAnalyzer/Program.cs
@@ -1,14 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace PokemonAnalyzer
 {
 	class Program
 	{
+		private const string PokeApi = "https://pokeapi.co/api/v2";
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int limit = Convert.ToInt32(args[0]);
+			int offset = Convert.ToInt32(args[1]);
+			
+			Console.WriteLine("Limit: " + limit + ". Offset: " + offset);
+			RequestPokemonData(limit, offset);
 			Console.WriteLine("Press enter exit");
 			Console.Read();
+		}
+
+		static void RequestPokemonData(int limit, int offset)
+		{
+			string uri = $"{PokeApi}/pokemon?limit={limit}&offset={offset}";
+			Console.WriteLine(Get(uri));
+		}
+		
+		
+		// Todo: find the source of this code. I got it from my BeatSaberUnzipper project, and unfortunately didn't save the link to the source
+		public static string Get(string uri)
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+			request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+			request.Proxy = null;
+
+			using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+			using(Stream stream = response.GetResponseStream())
+			using(StreamReader reader = new StreamReader(stream))
+			{
+				return reader.ReadToEnd();
+			}
 		}
 	}
 }
