32b293aaf78ce7f235230b9fb930e9efcd372d48
Robert Kasper <98301621+RHKasper@users.noreply.github.com>
2022-05-14 00:51:56 -0400

    Hello World setup for self-contained CLI app

---
+++ PokemonAnalyzer/PokemonAnalyzer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PokemonAnalyzer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("Press enter exit");
+			Console.Read();
+		}
+	}
+}
