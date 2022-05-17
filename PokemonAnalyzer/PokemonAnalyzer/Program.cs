e83bfd645f956f86f0170a4eba475a34062c3b93
Robert Kasper <98301621+RHKasper@users.noreply.github.com>
2022-05-16 23:38:51 -0400

    Switch to net6.0

--- PokemonAnalyzer/PokemonAnalyzer/Program.cs
+++ PokemonAnalyzer/PokemonAnalyzer/Program.cs
@@ -10,37 +10,45 @@ namespace PokemonAnalyzer
 		static void Main(string[] args)
 		{
 			Stopwatch stopwatch = Stopwatch.StartNew();
-			
+
 			int limit = args.Length > 0 ? Convert.ToInt32(args[0]) : 1000000;
 			int offset = args.Length > 1 ? Convert.ToInt32(args[1]) : 0;
-			
+
 			Console.WriteLine($"Downloading pokemon data from PokeAPI. limit = {limit}; offset = {offset}");
-			
-			PokemonListQuery pokemonListQuery = WebRequestManager.GetPokemonList(limit, offset);
-			List<PokemonData> pokemonDataList = GetPokemonDataList(pokemonListQuery.results);
-			
-			Console.WriteLine($"Download took {stopwatch.Elapsed.TotalSeconds} seconds.");
 
-			PokemonAnalysisData analysis = new PokemonAnalysisData(pokemonDataList);
-			
-			Console.WriteLine("\nAll Pokemon:");
-			Console.WriteLine($"\tAverage Height: {Math.Round(analysis.AvgHeight,1)}m");
-			Console.WriteLine($"\tAverage Weight: {Math.Round(analysis.AvgWeight,1)}Kg");
-
-			foreach (string typeName in analysis.AvgHeightByType.Keys)
+			try
 			{
-				string capitalizedTypeName = char.ToUpper(typeName[0]) + typeName.Substring(1);
-				Console.WriteLine($"\n{capitalizedTypeName}:");
-				Console.WriteLine($"\tAverage Height: {Math.Round(analysis.AvgHeightByType[typeName],1)}m");
-				Console.WriteLine($"\tAverage Weight: {Math.Round(analysis.AvgWeightByType[typeName],1)}Kg");
+				PokemonListQuery pokemonListQuery = WebRequestManager.GetPokemonList(limit, offset);
+				List<PokemonData> pokemonDataList = GetPokemonDataList(pokemonListQuery.results);
+
+				Console.WriteLine($"Download took {stopwatch.Elapsed.TotalSeconds} seconds.");
+
+				PokemonAnalysisData analysis = new PokemonAnalysisData(pokemonDataList);
+
+				Console.WriteLine("\nAll Pokemon:");
+				Console.WriteLine($"\tAverage Height: {Math.Round(analysis.AvgHeight, 1)}m");
+				Console.WriteLine($"\tAverage Weight: {Math.Round(analysis.AvgWeight, 1)}Kg");
+
+				foreach (string typeName in analysis.AvgHeightByType.Keys)
+				{
+					string capitalizedTypeName = char.ToUpper(typeName[0]) + typeName.Substring(1);
+					Console.WriteLine($"\n{capitalizedTypeName}:");
+					Console.WriteLine($"\tAverage Height: {Math.Round(analysis.AvgHeightByType[typeName], 1)}m");
+					Console.WriteLine($"\tAverage Weight: {Math.Round(analysis.AvgWeightByType[typeName], 1)}Kg");
+				}
+			}
+			catch
+			{
+				Console.WriteLine("Downlaoding or processing data failed");
+				throw;
 			}
 
 			Console.WriteLine($"\nProgram took {stopwatch.Elapsed.TotalSeconds} seconds.");
-			
+
 			Console.WriteLine("Press Enter to exit");
 			Console.ReadLine();
 		}
-		
+
 
 		private static List<PokemonData> GetPokemonDataList(List<PokemonEntry> pokemonEntries)
 		{
